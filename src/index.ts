a0a735ac85c01989944efae78743897f1236a41c
Oksana Moroz <k.moroz.mail@gmail.com>
2024-01-10 21:19:23 +0200

    final

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,72 @@
 //Створіть тип, який об'єднує властивості двох об'єктів тільки в тому випадку, якщо їхні значення мають спільний тип.
 
 //Наприклад: { a: number; b: string } та { b: string; c: boolean } => { b: string; }
+
+interface Book {
+  title: string;
+  author: string;
+  genre: string;
+}
+
+type BookWithPages = {
+  title: string;
+  author: string;
+  genre: string;
+  pages: number;
+};
+
+//1
+type FuncParam<T extends (...params: any[]) => any> = T extends (
+  ...params: any[]
+) => any
+  ? ReturnType<T>
+  : never;
+
+function getBookTitle(book: Book): string {
+  return book.title;
+}
+const book: Book = {
+  title: "Кобзар",
+  author: "Тарас Шевченко",
+  genre: "Вірші",
+};
+const bookTitle: FuncParam<typeof getBookTitle> = getBookTitle(book);
+
+//2
+type FuncnInfo<T extends (param: any) => any> = T extends (
+  param: infer ParameterType
+) => infer ReturnType
+  ? [ReturnType, ParameterType]
+  : never;
+
+type GetBookTitleInfo = FuncnInfo<typeof getBookTitle>;
+
+const bookInfo: GetBookTitleInfo = [
+  "Вірші",
+  {
+    title: "Кобзар",
+    author: "Тарас Шевченко",
+    genre: "Вірші",
+  },
+];
+
+//3
+type IntersectingProp<T, U> = {
+  [K in Extract<keyof T, keyof U>]: T[K] & U[K];
+};
+
+const bookWithPages: BookWithPages = {
+  title: "Кобзар",
+  author: "Тарас Шевченко",
+  genre: "Вірші",
+  pages: 200,
+};
+
+const mergedBook: IntersectingProp<Book, BookWithPages> = {
+  ...book,
+  ...bookWithPages,
+};
+
+console.log(bookTitle);
+console.log(bookInfo);
+console.log(mergedBook);
